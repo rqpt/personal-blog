a05731c49486a11bb0495cefd8b45edf3a02f662
Ernst <ernst@cubezoo.joburg>
2024-07-19 17:29:03 +0200

    Improve seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,28 +13,30 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $bodiesRequired = 3;
+        $uniquePostsRequired = 3;
 
-        $markdownResponses = Http::pool(function (Pool $pool) use ($bodiesRequired) {
+        $markdownResponses = Http::pool(function (Pool $pool) use ($uniquePostsRequired) {
             $ocean = [];
 
-            for ($i = 0; $i < $bodiesRequired; $i++) {
+            for ($i = 0; $i < $uniquePostsRequired; $i++) {
                 $ocean[] = $pool->get(config('third-party-api.random_markdown'));
             }
 
             return $ocean;
         });
 
-        Post::factory($bodiesRequired, [
-            'markdown' => $markdownResponses[0],
-        ])->published()->create();
+        for ($i = 0; $i < $uniquePostsRequired; $i++) {
+            Post::factory(state: [
+                'markdown' => $markdownResponses[$i],
+            ])->published()->create();
 
-        Post::factory($bodiesRequired, [
-            'markdown' => $markdownResponses[1],
-        ])->published()->withTableOfContents()->create();
+            Post::factory(state: [
+                'markdown' => $markdownResponses[$i],
+            ])->published()->withTableOfContents()->create();
 
-        Post::factory($bodiesRequired, [
-            'markdown' => $markdownResponses[2],
-        ])->published()->withTableOfContents()->withAnEmbeddedVideo()->create();
+            Post::factory(state: [
+                'markdown' => $markdownResponses[$i],
+            ])->published()->withTableOfContents()->withAnEmbeddedVideo()->create();
+        }
     }
 }
