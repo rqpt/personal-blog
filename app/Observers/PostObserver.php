4b98a1157f325288ab64b14801da086e49f02985
Ernst <ernst@cubezoo.joburg>
2024-07-19 10:51:01 +0200

    Simplify observer

--- app/Observers/PostObserver.php
+++ app/Observers/PostObserver.php
@@ -3,51 +3,25 @@
 namespace App\Observers;
 
 use App\Models\Post;
-use GrahamCampbell\Markdown\Facades\Markdown;
 use Illuminate\Support\Facades\Storage;
 
 class PostObserver
 {
-    public function creating(Post $post): void
-    {
-        $localBackupFilename = $post->getBackupFilename();
-
-        $this->fetchOriginalAndStoreHtml($localBackupFilename, $post);
-    }
-
     public function updating(Post $post): void
     {
         $originalPost = $post->getOriginal();
 
-        $localBackupFilename = $post->getBackupFilename(
+        $originalBackupFilename = $post->getBackupFilename(
             $originalPost['title'],
         );
 
-        $this->fetchOriginalAndStoreHtml($localBackupFilename, $post);
-
-        if ($post->isDirty('title')) {
-            $newLocalBackupFilename = $post->getBackupFilename();
-
-            Storage::disk('backup')->move(
-                $localBackupFilename,
-                $newLocalBackupFilename,
-            );
-        }
+        Storage::delete($originalBackupFilename);
     }
 
     public function deleted(Post $post): void
     {
         $localBackupFilename = $post->getBackupFilename();
 
-        Storage::disk('backup')->delete($localBackupFilename);
-    }
-
-    private function fetchOriginalAndStoreHtml(
-        string $localBackupFilename,
-        Post $post,
-    ): void {
-        $markdown = Storage::disk('backup')->get($localBackupFilename);
-
-        $post->body = Markdown::convert($markdown)->getContent();
+        Storage::delete($localBackupFilename);
     }
 }
