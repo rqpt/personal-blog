9a1f56e785d8a01be72cc368b68b8dd419494d10
Ernst <ernst@cubezoo.joburg>
2024-07-20 19:50:46 +0200

    Refactor ugly if statement

--- app/Markdown/TorchlightNodeRendererExtension.php
+++ app/Markdown/TorchlightNodeRendererExtension.php
@@ -22,16 +22,16 @@ class TorchlightNodeRendererExtension implements ExtensionInterface, NodeRendere
     {
         $lang = $node->getInfo();
 
-        if (is_null($lang)) {
-            $langAttribute = '';
-        } else {
-            $langAttribute = " language='$lang'";
-        }
+        $langAttribute = is_null($lang) ? '' : " language='$lang";
 
         $torchlightOpeningTag = "<pre><x-torchlight-code{$langAttribute}>\n";
 
         $torchlightClosingTag = '</x-torchlight-code></pre>';
 
-        return BladeManager::renderContent(Blade::render($torchlightOpeningTag.$node->getLiteral().$torchlightClosingTag));
+        return BladeManager::renderContent(
+            Blade::render(
+                $torchlightOpeningTag.$node->getLiteral().$torchlightClosingTag,
+            ),
+        );
     }
 }
