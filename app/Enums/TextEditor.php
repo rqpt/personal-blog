fd6c5a5bcea8e57220dee58136e4e46eb3bb4171
Ernst <ernst@cubezoo.joburg>
2024-07-20 16:27:19 +0200

    Add sensible defaults

--- app/Enums/TextEditor.php
+++ app/Enums/TextEditor.php
@@ -32,4 +32,20 @@ enum TextEditor: string
     {
         return 'Select your preferred text editor for the post body.';
     }
+
+    public function editorSensibleDefaultSettings(): string
+    {
+        if ($this === self::VIM) {
+            return <<<EOD
+                +set numberwidth=3 shiftwidth=2 tabstop=2 softtabstop=2 expandtab
+                smartindent number relativenumber ignorecase smartcase breakindent
+                splitbelow splitright scrolloff=20 sidescrolloff=15 confirm
+                noswapfile wildmode=longest:full,full isfname+=@-@ lazyredraw
+                completeopt=menuone,longest,preview incsearch updatetime=50
+                showmode conceallevel=2 foldlevel=99 cmdheight=1
+                EOD;
+        } else {
+            return '';
+        }
+    }
 }
