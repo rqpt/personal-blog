5d855b3ea8762b025ac85828247dcd0dd7e66a29
Ernst <ernst@cubezoo.joburg>
2024-07-20 08:17:05 +0200

    Simplify index view

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -44,82 +44,9 @@ render(function ($view) {
         @endforeach
     </ul>
 
-    @if(count($posts) == 0)
-        <div
-        x-data="{
-            intro: true,
-            firstParagraph: false,
-            secondParagraph: false,
-            solutionButton: false,
-            help: false,
-        }"
-        x-init="
-        setTimeout(() => firstParagraph = true, 1000);
-        setTimeout(() => secondParagraph = true, 3000);
-        setTimeout(() => solutionButton = true, 5000);
-        "
-        >
-            <div
-            x-show="intro"
-            x-transition
-            x-transition:leave.duration.0ms
-            >
-                <p
-                x-cloak
-                x-show="firstParagraph"
-                x-transition
-                >
-                    Oh no, it looks like we dont have any posts...
-                </p>
-
-                <p
-                x-cloak
-                x-show="secondParagraph"
-                x-transition
-                >
-                    What ever shall we do? 🤔
-                </p>
-
-                <button
-                x-cloak
-                x-show="solutionButton"
-                x-transition
-                @click="intro = false; help = true"
-                >
-                    💡
-                </button>
-            </div>
-
-            <div
-            x-cloak
-            x-show="help"
-            x-transition
-            >
-                <p>
-                    Run the following command, and hit refresh.
-                </p>
-
-                <pre><x-torchlight-code language='curl'>
-                    curl http://127.0.0.1:80/api/post \
-                        -d "title"="Hello World" \
-                        -d "body"="# Hi there" \
-                        -d "published"=true
-                </x-torchlight-code></pre>
-
-                <p>
-                    No worries if you made a mistake - simply run the next
-                    command to delete the post and try again.
-                </p>
-
-                <p>
-                    You should be provided with the postId by the output of the
-                    previous command.
-                </p>
-
-                <pre><x-torchlight-code language='curl'>
-                    curl -X DELETE http://127.0.0.1:80/api/example-post-id-5
-                </x-torchlight-code></pre>
-            </div>
-        </div>
+    @if ($posts->count() == 0)
+        <p>
+            No posts found
+        </p>
     @endif
 </x-layout.app>
