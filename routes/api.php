d5b4c684de8bd9027673b6b8fb0fa9d188c65105
Ernst <ernst@cubezoo.joburg>
2024-08-08 20:55:02 +0200

    comment out api routes for now

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('posts', PostController::class);
+// Route::apiResource('posts', PostController::class);
