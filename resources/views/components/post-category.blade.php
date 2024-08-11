ccbbaefee68192d6366514d7640d60787200b64f
Ernst <ernst@cubezoo.joburg>
2024-08-11 23:04:19 +0200

    Improve style and UX

--- resources/views/components/post-category.blade.php
+++ resources/views/components/post-category.blade.php
@@ -4,44 +4,46 @@
         @foreach($posts as $post)
             @php $linkRef = "link-{$loop->iteration}"; @endphp
 
-            <article
+            <a
             x-data="{ clicked: false }"
-            :aria-busy="clicked"
-            :aria-label="'Please wait...'"
+            id="{{ $linkRef }}"
+            x-ref="{{ $linkRef }}"
+            @click.capture="clicked = true"
+            @keydown.capture.enter="clicked = true; localStorage.setItem('lastFocusedLink', '{{ $linkRef }}')"
+            @mouseenter="$focus.focus($el); localStorage.setItem('lastFocusedLink', '{{ $linkRef }}')"
+            @mouseenter.debounce="localStorage.setItem('lastFocusedLink', '{{ $linkRef }}')"
+            wire:navigate.hover
+            href="/{{ $post->urlSlug() }}"
             >
-                <header>
-                    <a
+                <article
+                :aria-busy="clicked"
+                :aria-label="'Please wait...'"
+                >
+                    <hgroup
                     x-show="!clicked"
-                    id="{{ $linkRef }}"
-                    x-ref="{{ $linkRef }}"
-                    @click.capture="clicked = true"
-                    @keydown.capture.enter="clicked = true; localStorage.setItem('lastFocusedLink', '{{ $linkRef }}')"
-                    @mouseenter="$focus.focus($el); localStorage.setItem('lastFocusedLink', '{{ $linkRef }}')"
-                    @mouseenter.debounce="localStorage.setItem('lastFocusedLink', '{{ $linkRef }}')"
-                    wire:navigate.hover
-                    href="/{{ $post->urlSlug() }}"
                     >
-                        <strong>
+                        <h3>
                             {{ $post->title }}
-                        </strong>
-                    </a>
-                </header>
+                        </h3>
 
-                <small>
-                    {{ $post->frontmatter->description }}
-                </small>
+                        <p>
+                            <small>
+                                {{ $post->frontmatter->description }}
+                            </small>
+                        </p>
+                    </hgroup>
 
-                <footer>
-                    <small>
-                        <em
-                        data-tooltip="{{ $post->timestampTooltip() }}"
-                        >
-                            {{ $post->timestamps() }}
-                        </em>
-                    </small>
-                </footer>
-            </article>
-
+                    <footer>
+                        <small>
+                            <em
+                            data-tooltip="{{ $post->timestampTooltip() }}"
+                            >
+                                {{ $post->timestamps() }}
+                            </em>
+                        </small>
+                    </footer>
+                </article>
+            </a>
         @endforeach
     </section>
 @endif
