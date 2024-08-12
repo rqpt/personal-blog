755514399da5d61f1470314fbb98e4572cc80d3d
Ernst <ernst@cubezoo.joburg>
2024-08-12 12:54:08 +0200

    Add x-cloak

--- resources/views/components/post-category.blade.php
+++ resources/views/components/post-category.blade.php
@@ -35,6 +35,7 @@
                             class="mobile-hidden"
                             x-show="focused"
                             x-collapse
+                            x-cloak
                             >
                                 <small>
                                     {{ $post->frontmatter->description }}
